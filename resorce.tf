908bd559f5984f1c20960e7bf2382cd21da86c8e
navya-sai-17 <navyasaidanaboina17@gmail.com>
2025-07-16 09:50:49 +0000

    spoil

--- resorce.tf
+++ resorce.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "storeterra" {
 }
  
 resource "azurerm_storage_account" "saterra" {
-    name                     = "terrastorage1715"
+    name                     = "terrastorage171503"
     resource_group_name      = azurerm_resource_group.storeterra.name
     location                 = azurerm_resource_group.storeterra.location
     account_tier             = "Standard"
